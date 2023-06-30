09b768845a484d655dd6a89bdb12f00dda92cb03
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-06-30 15:27:38 -0500

    made changes to cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,17 +2,28 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//create a array and call it basket
 let basket =[];
+
 function addItem(newItem){
+    //add new items to the array
     basket.push(newItem)
     return true;
-}
+}//end addItem 
+
+//call the function to see if it works 
+console.log('add milk to my basket', addItem('pop'));
 console.log('add eggs to my basket', addItem('eggs'));
-console.log(basket);
+console.log('Basket is now',basket);
 
+function listItems(){
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i], '\n');
+    }//end for loop
+}//end listItems 
 
-
-
+//call the function to see if it works
+console.log('What is in my basket:', listItems());
 
 
 
